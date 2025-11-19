a6cf664ae607bbac6c418bb74d3e7db316bc156e
flych3r <matheus.sampaio011@gmail.com>
2025-11-19 10:29:53 -0300

    updated logging json

--- source/logging_config.py
+++ source/logging_config.py
@@ -5,28 +5,37 @@ import sys
 
 
 class JsonFormatter(logging.Formatter):
+
     def format(self, record: logging.LogRecord) -> str:
         log_entry = {
             "severity": record.levelname,
             "message": record.getMessage(),
-            "name": record.name,
-            "timestamp": self.formatTime(record, self.datefmt)
+            "logger": record.name,
+            "timestamp": self.formatTime(record, self.datefmt),
         }
+
+        if "CLOUD_RUN_JOB" in os.environ:
+            log_entry["job"] = os.getenv("CLOUD_RUN_JOB")
+            log_entry["execution"] = os.getenv("CLOUD_RUN_EXECUTION")
+            log_entry["taskIndex"] = os.getenv("CLOUD_RUN_TASK_INDEX")
+
         if record.exc_info:
-            log_entry["message"] += "\n" + self.formatException(record.exc_info)
+            log_entry["stack"] = self.formatException(record.exc_info)
+
         return json.dumps(log_entry)
 
 
-def setup_root_logger(level: int = logging.INFO) -> None:
-    root_logger = logging.getLogger()
+def setup_root_logger(level=logging.INFO):
+    root = logging.getLogger()
 
-    if root_logger.hasHandlers():
-        root_logger.handlers.clear()
+    if root.hasHandlers():
+        root.handlers.clear()
 
-    root_logger.setLevel(level)
+    root.setLevel(level)
+
     handler = logging.StreamHandler(sys.stdout)
 
-    if "CLOUD_RUN_JOB" in os.environ:
+    if "K_SERVICE" in os.environ or "CLOUD_RUN_JOB" in os.environ:
         formatter = JsonFormatter()
     else:
         formatter = logging.Formatter(
@@ -34,4 +43,4 @@ def setup_root_logger(level: int = logging.INFO) -> None:
         )
 
     handler.setFormatter(formatter)
-    root_logger.addHandler(handler)
+    root.addHandler(handler)
